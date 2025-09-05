23355d5a814b4aa25139b798028ddd3cfb704cba
erenworld <erenxturkoglu@gmail.com>
2025-09-05 20:15:25 +0200

    Add test for different color

--- color_test.go
+++ color_test.go
@@ -1,26 +1,33 @@
 package chalk
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"time"
-
 	"testing"
 )
 
 func TestColor(t *testing.T) {
-	go func() {
-		scanner := bufio.NewScanner(os.Stdout)
-		for scanner.Scan() {
-			fmt.Println(scanner.Text()) // Println will add back the final '\n'
-		}
-	}()
+	Cyan.Print("Prints text in cyan.")
+	Blue.Print("Prints text in blue.")
 
-	time.Sleep(time.Millisecond * 300)
+	// Chain SGR parameters
+	Green.Add(Bold).Println("Green with bold.")
+	Red.Add(BgWhite, Underline).Printf("Red with White background and underscore: %s\n", "format too!")
 
-	c := Red.Bold()
-	c.Println("Afyon")
+	c := Cyan.Add(Underline)
+	c.Println("Prints bold cyan.")
+	c.Printf("Thir prints bold cyan %s\n", "too!.")
 
+	// Create custom color objects:
+	d := New(FgGreen, BgCyan, Italic)
+	d.Print("Italic green with cyan background")
 
+	Cyan.Set()
+	fmt.Println("Existing text in your codebase will be now in Cyan")
+	fmt.Printf("This one %s\n", "too")
+	Unset()
+
+	New(FgBlack, BgWhite, Bold).Set()
+	defer Unset()
+
+	fmt.Println("All text will be now bold red with white background.")
 }
