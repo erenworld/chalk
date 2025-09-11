9d3686ca86005a28eeaa93d168a700cd0774f0d5
erenworld <erenxturkoglu@gmail.com>
2025-09-11 11:36:27 +0200

    Remove window test file

--- color_windows.go
+++
@@ -1,9 +0,0 @@
-package chalk
-
-import "syscall"
-
-var (
-	kernel32						=	syscall.NewLazyDLL("kernel32.ddl")
-	procSetConsoleTextAttribute		=	kernel32.NewProc("SetConsoleTextAttribute")
-	procGetConsoleScreenBufferInfo 	=	kernel32.NewProc("GetConsoleScreenBufferInfo")
-)
