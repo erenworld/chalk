b07451e0203e5de766a285b4c25f03234671b306
erenworld <erenxturkoglu@gmail.com>
2025-09-21 15:27:54 +0200

    Added new visual tests

--- color_test.go
+++ color_test.go
@@ -4,17 +4,15 @@ import (
 	"bytes"
 	"fmt"
 	"os"
-	"runtime"
 	"testing"
 
 	"github.com/shiena/ansicolor"
 )
 
+// Testing colors is kinda different. First we test for given colors and their
+// escaped formatted results. Next we create some visual tests to be tested.
+// Each visual test includes the color name to be compared.
 func TestColor(t *testing.T) {
-	if runtime.GOOS == "windows" {
-		t.Skip("Windows is not supported yet.")
-	}
-
 	rb := new(bytes.Buffer)
 	Output = rb
 
@@ -46,8 +44,42 @@ func TestColor(t *testing.T) {
 			t.Errorf("Expecting %s, got '%s'\n", escapedForm, scannedLine)
 		}
 	}
+}
 
-	// Visual Test
+func TestNoColor(t *testing.T) {
+	rb := new(bytes.Buffer)
+	Output = rb
+
+	testColors := []struct {
+		text string
+		code Attribute
+	}{
+		{text: "black", code: FgBlack},
+		{text: "red", code: FgRed},
+		{text: "green", code: FgGreen},
+		{text: "yellow", code: FgYellow},
+		{text: "blue", code: FgBlue},
+		{text: "magent", code: FgMagenta},
+		{text: "cyan", code: FgCyan},
+		{text: "white", code: FgWhite},
+	}
+
+	for _, c := range testColors {
+		p := New(c.code)
+		p.DisableColor()
+		p.Print(c.text)
+
+		line, _ := rb.ReadString('\n')
+
+		if line != c.text {
+			t.Errorf("Expecting %s, got '%s'\n", c.text, line)
+		}
+	}
+
+}
+
+func TestColorVisual(t *testing.T) {
+	// First Visual Test
 	fmt.Println("")
 	Output = ansicolor.NewAnsiColorWriter(os.Stdout)
 
@@ -80,9 +112,15 @@ func TestColor(t *testing.T) {
 	New(FgWhite, Bold).Println(" white")
 	fmt.Println("")
 
+	// Second Visual test
 	Black("black")
 	Red("red")
 	Green("green")
+	Yellow("yellow")
+	Blue("blue")
+	Magenta("magenta")
+	Cyan("cyan")
+	White("white")
 
 	// Third visual test
 	fmt.Println()
@@ -107,11 +145,12 @@ func TestColor(t *testing.T) {
 
 	fmt.Fprintf(Output, "this is a %s and this is %s.\n", put("warning"), warn("error"))
 
-	info := New(FgRed, BgYellow).SprintFunc()
+	info := New(FgWhite, BgGreen).SprintFunc()
 	fmt.Fprintf(Output, "this %s rocks!\n", info("package"))
 
 	// Fifth Visual Test
 	fmt.Println()
+
 	fmt.Fprintln(Output, BlackString("black"))
 	fmt.Fprintln(Output, RedString("red"))
 	fmt.Fprintln(Output, GreenString("green"))
@@ -120,37 +159,4 @@ func TestColor(t *testing.T) {
 	fmt.Fprintln(Output, MagentaString("magenta"))
 	fmt.Fprintln(Output, CyanString("cyan"))
 	fmt.Fprintln(Output, WhiteString("white"))
-}
-
-
-func TestNoColor(t *testing.T) {
-	rb := new(bytes.Buffer)
-	Output = rb
-
-	testColors := []struct {
-		text string
-		code Attribute
-	}{
-		{text: "black", code: FgBlack},
-		{text: "red", code: FgRed},
-		{text: "green", code: FgGreen},
-		{text: "yellow", code: FgYellow},
-		{text: "blue", code: FgBlue},
-		{text: "magent", code: FgMagenta},
-		{text: "cyan", code: FgCyan},
-		{text: "white", code: FgWhite},
-	}
-
-	for _, c := range testColors {
-		p := New(c.code)
-		p.DisableColor()
-		p.Print(c.text)
-
-		line, _ := rb.ReadString('\n')
-
-		if line != c.text {
-			t.Errorf("Expecting %s, got '%s'\n", c.text, line)
-		}
-	}
-
 }
